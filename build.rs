be90e2a1945743f4c4d447bc6c2d04737e7af559
Abass Hammed <139161544+AbassHammed@users.noreply.github.com>
2025-07-29 18:54:32 +0200

    Fixed actions errors

--- build.rs
+++ build.rs
@@ -2,6 +2,7 @@ const COMMANDS: &[&str] = &["share_text", "share_data", "share_file"];
 
 fn main() {
   tauri_plugin::Builder::new(COMMANDS)
+    .global_api_script_path("./api-iife.js")
     .android_path("android")
     .ios_path("ios")
     .build();
